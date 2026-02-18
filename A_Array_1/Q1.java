a5e94439a21e337d0cb274458a483969035b3313
SHEKH DALIM <156339532+Shekh-Dalim@users.noreply.github.com>
2026-02-18 17:10:03 +0530

    Update and rename Q1.c++ to Q1.java

--- A_Array_1/Q1.java
+++ A_Array_1/Q1.java
@@ -3,7 +3,34 @@
 
 /*-----------------  Using loop  TC-> O(n)  SC->(1) -----------------*/
 
-// we know
+import java.util.Scanner;
+
+class Dalim {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void inputEle(int arr[], int size) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+
+    public static void printArr(int arr[],int size){
+        for(int i =0; i< size;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.print("Enter the size: ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the array element: ");
+        inputEle(arr, size);
+        System.out.println("Display the array Element: ");
+        printArr(arr, size);
+    }
+}
+
 
 /*-------------- using Recurtion Relation  TC-> O(n)  SC->(n) -------------*/
 
